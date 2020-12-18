2608354682a6bbe977e019237bf164988c668085
Atila <atila.romao@hotmail.com>
2020-12-18 08:30:54 -0300

    Add project files.

---
+++ ExercicioEncapsulamento/Conta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExercicioEncapsulamento
+{
+    internal class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numeroConta, string titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+        public Conta(int numeroConta, string titular, double depositoInicial) : this(numeroConta, titular) // por meio deste this eu acesso o outro construtor.
+        {
+            this.Depositar(depositoInicial);
+        }
+
+        public void Sacar(double valor)
+        {
+            if (Saldo > valor)
+            {
+                Saldo -= valor + 5;
+                Console.WriteLine($"Dados da conta atualizados: ");
+                Console.WriteLine(this.ToString());
+            }
+            else
+            {
+                Console.WriteLine($"Valor solicitado: {valor} maior que saldo: {Saldo}");
+            }
+        }
+
+        public void SaldoInicial(double saldoInicial)
+        {
+            Saldo += saldoInicial;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+            Console.WriteLine($"Dados da conta atualizados: ");
+            Console.WriteLine(this.ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ExercicioEncapsulamento/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioEncapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o numero da conta: ");
+            int numeroConta = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Entre o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.Write("Havera deposito inicial: ");
+            char comDepositoInicial = Convert.ToChar(Console.ReadLine());
+
+            Conta c1 = new Conta(numeroConta, nomeTitular);
+
+            if (comDepositoInicial == 's')
+            {
+                Console.Write("Entre o valor de deposito inicial: ");
+                double saldoInicial = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                c1.SaldoInicial(saldoInicial);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(c1);
+
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para deposito: ");
+            double deposito = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c1.Depositar(deposito);
+
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            double saque = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c1.Sacar(saque);
+
+
+        }
+    }
+}
